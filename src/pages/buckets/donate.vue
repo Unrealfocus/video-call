6d1583ea59e6577b3c237a16171646149678e77d
Victor Edet <57493308+victoredet@users.noreply.github.com>
2023-06-24 20:35:20 +0000

    fix donate page

--- src/pages/buckets/donate.vue
+++ src/pages/buckets/donate.vue
@@ -22,14 +22,14 @@
             </div>
           </div>
         </div>
-        <!-- second slide  -->
+
         <div class="form">
           <div class="py-4 space-y-[20px]">
-            <p class="font-poppins font-[600] text-[18px]">
-              Tip Puthand services
-            </p>
-
-            <div class="">
+            <!-- first slide  -->
+            <div class="" :class="[currentPage == 1 ? '' : 'hidden']">
+              <p class="font-poppins font-[600] text-[18px]">
+                Tip Puthand services
+              </p>
               <p class="text-[#484848] text-[18px] font-[600] font-poppins">
                 Enter your donation amount:
               </p>
@@ -45,142 +45,167 @@
                   pattern="[0-9]*"
                   class="bg-[#fff] w-full border-none bg-transparent outline-none rounded full text-[31px] text-[#939393] font-[600]" />
               </div>
-            </div>
-            <div class="">
-              <p class="text-[#484848] text-[18px] font-[600] font-poppins">
-                Enter tip amount:
-              </p>
-              <div
-                class="w-full border-2 border-[#93939] rounded-2xl p-3 flex space-x-[10px]">
+              <div class="">
+                <p class="text-[#484848] text-[18px] font-[600] font-poppins">
+                  Enter tip amount:
+                </p>
                 <div
-                  class="bg-[#EAF9F0] flex justify-center items-center rounded w-[40px]">
-                  <p class="text-[#176D3A] font-[800] text-[25px]">₦</p>
-                </div>
-                <input
-                  v-model="tip"
-                  type="number"
-                  pattern="[0-9]*"
-                  class="bg-[#fff] w-full border-none bg-transparent outline-none rounded full text-[31px] text-[#939393] font-[600]" />
-              </div>
-              <p class="text-[#939393] text-[14px] font-[500]">
-                * Puthand will continue offering its services thanks to donors
-                who will leave an optional amount here:
-              </p>
-            </div>
-            <div class="space-y-[10px]">
-              <p class="text-[#484848] text-[18px] font-[600] font-poppins">
-                Tell us about yourself
-              </p>
-              <div
-                class="w-full border-2 border-[#93939] rounded-2xl p-3 flex space-x-[10px]">
-                <div class="flex justify-center items-center rounded">
-                  <div class="border border-[#295F2D] rounded-full p-1">
-                    <div
-                      class="bg-[#295F2D] w-[10px] h-[10px] rounded-full"></div>
+                  class="w-full border-2 border-[#93939] rounded-2xl p-3 flex space-x-[10px]">
+                  <div
+                    class="bg-[#EAF9F0] flex justify-center items-center rounded w-[40px]">
+                    <p class="text-[#176D3A] font-[800] text-[25px]">₦</p>
                   </div>
+                  <input
+                    v-model="tip"
+                    type="number"
+                    pattern="[0-9]*"
+                    class="bg-[#fff] w-full border-none bg-transparent outline-none rounded full text-[31px] text-[#939393] font-[600]" />
                 </div>
-                <input
-                  class="bg-[#fff] w-full border-none bg-transparent outline-none rounded full text-[21px] text-[#939393] font-[600]/"
-                  placeholder="Email"
-                  v-model="email" />
-              </div>
-              <div
-                class="w-full border-2 border-[#93939] rounded-2xl p-3 flex space-x-[10px]">
-                <div class="flex justify-center items-center rounded">
-                  <div class="border border-[#295F2D] rounded-full p-1">
-                    <div
-                      class="bg-[#295F2D] w-[10px] h-[10px] rounded-full"></div>
-                  </div>
-                </div>
-                <input
-                  class="bg-[#fff] w-full border-none bg-transparent outline-none rounded full text-[21px] text-[#939393] font-[600]/"
-                  placeholder="Full Name"
-                  v-model="name" />
-              </div>
-              <div
-                class="w-full border-2 border-[#93939] rounded-2xl p-3 flex space-x-[10px]">
-                <div class="flex justify-center items-center rounded">
-                  <div class="border border-[#295F2D] rounded-full p-1">
-                    <div
-                      class="bg-[#295F2D] w-[10px] h-[10px] rounded-full"></div>
-                  </div>
-                </div>
-                <input
-                  class="bg-[#fff] w-full border-none bg-transparent outline-none rounded full text-[21px] text-[#939393] font-[600]/"
-                  placeholder="Phone Number"
-                  v-model="phone" />
-              </div>
-            </div>
-            <div class="">
-              <p class="text-[#484848] text-[18px] font-[600] font-poppins">
-                Payment Method
-              </p>
-              <div
-                class="w-full border-2 border-[#93939] rounded-2xl p-3 flex space-x-[10px]">
-                <div class="flex justify-center items-center rounded">
-                  <div class="border border-[#295F2D] rounded-full p-1">
-                    <div
-                      class="bg-[#295F2D] w-[10px] h-[10px] rounded-full"></div>
-                  </div>
-                </div>
-                <div
-                  class="bg-[#fff] w-full border-none bg-transparent outline-none rounded full text-[21px] text-[#939393] font-[600]">
-                  Paystack
-                </div>
-              </div>
-            </div>
-            <div class="">
-              <p class="text-[#242424] text-[18px] font-[600] font-poppins">
-                Your donation
-              </p>
-              <div class="w-full border-t-2 border-b-2 border-[#000] py-3">
-                <div class="flex justify-between">
-                  <p class="text-[#939393] font-poppins font-[600]">
-                    Your donation
-                  </p>
-                  <p class="font-poppins text-[21px] font-[700] text-[#242424]">
-                    ₦{{ amount == "" ? 0 : parseInt(amount) }}
-                  </p>
-                </div>
-                <div class="flex justify-between">
-                  <p class="text-[#939393] font-poppins font-[600]">
-                    Puthand Tip
-                  </p>
-                  <p class="font-poppins text-[21px] font-[700] text-[#242424]">
-                    ₦{{ tip == "" ? 0 : parseInt(tip) }}
-                  </p>
-                </div>
-              </div>
-              <div class="flex justify-between">
-                <p class="text-[#939393] font-poppins font-[600]">Total due</p>
-                <p class="font-poppins text-[21px] font-[700] text-[#242424]">
-                  ₦{{ parseInt(amount) + parseInt(tip) || 0 }}
+                <p class="text-[#939393] text-[14px] font-[500]">
+                  * Puthand will continue offering its services thanks to donors
+                  who will leave an optional amount here:
                 </p>
               </div>
             </div>
-            <div class="">
-              <p class="text-[#242424] text-[18px] font-[600] font-poppins">
-                Donation Details (Optional)
-              </p>
 
-              <div class="w-full py-3">
-                <div class="flex items-center space-x-[10px]">
-                  <div class="cursor-pointer">
-                    <div class="w-[20px] h-[20px] border rounded"></div>
+            <!-- page 2  -->
+            <div class="" :class="[currentPage == 2 ? '' : 'hidden']">
+              <div class="space-y-[10px]">
+                <p class="text-[#484848] text-[18px] font-[600] font-poppins">
+                  Tell us about yourself
+                </p>
+                <div
+                  class="w-full border-2 border-[#93939] rounded-2xl p-3 flex space-x-[10px]">
+                  <div class="flex justify-center items-center rounded">
+                    <div class="border border-[#295F2D] rounded-full p-1">
+                      <div
+                        class="bg-[#295F2D] w-[10px] h-[10px] rounded-full"></div>
+                    </div>
                   </div>
-                  <p class="font-poppins font-[500] text-[#999999]">
-                    Don’t display my name publicly on campaign
+                  <input
+                    class="bg-[#fff] w-full border-none bg-transparent outline-none rounded full text-[21px] text-[#939393] font-[600]/"
+                    placeholder="Email"
+                    v-model="email" />
+                </div>
+                <div
+                  class="w-full border-2 border-[#93939] rounded-2xl p-3 flex space-x-[10px]">
+                  <div class="flex justify-center items-center rounded">
+                    <div class="border border-[#295F2D] rounded-full p-1">
+                      <div
+                        class="bg-[#295F2D] w-[10px] h-[10px] rounded-full"></div>
+                    </div>
+                  </div>
+                  <input
+                    class="bg-[#fff] w-full border-none bg-transparent outline-none rounded full text-[21px] text-[#939393] font-[600]/"
+                    placeholder="Full Name"
+                    v-model="name" />
+                </div>
+                <div
+                  class="w-full border-2 border-[#93939] rounded-2xl p-3 flex space-x-[10px]">
+                  <div class="flex justify-center items-center rounded">
+                    <div class="border border-[#295F2D] rounded-full p-1">
+                      <div
+                        class="bg-[#295F2D] w-[10px] h-[10px] rounded-full"></div>
+                    </div>
+                  </div>
+                  <input
+                    class="bg-[#fff] w-full border-none bg-transparent outline-none rounded full text-[21px] text-[#939393] font-[600]/"
+                    placeholder="Phone Number"
+                    v-model="phone" />
+                </div>
+              </div>
+            </div>
+            <!-- third slide  -->
+            <div class="" :class="[currentPage == 3 ? '' : 'hidden']">
+              <div class="">
+                <p class="text-[#484848] text-[18px] font-[600] font-poppins">
+                  Payment Method
+                </p>
+                <div
+                  class="w-full border-2 border-[#93939] rounded-2xl p-3 flex space-x-[10px]">
+                  <div class="flex justify-center items-center rounded">
+                    <div class="border border-[#295F2D] rounded-full p-1">
+                      <div
+                        class="bg-[#295F2D] w-[10px] h-[10px] rounded-full"></div>
+                    </div>
+                  </div>
+                  <div
+                    class="bg-[#fff] w-full border-none bg-transparent outline-none rounded full text-[21px] text-[#939393] font-[600]">
+                    Paystack
+                  </div>
+                </div>
+              </div>
+              <div class="">
+                <p class="text-[#242424] text-[18px] font-[600] font-poppins">
+                  Your donation
+                </p>
+                <div class="w-full border-t-2 border-b-2 border-[#000] py-3">
+                  <div class="flex justify-between">
+                    <p class="text-[#939393] font-poppins font-[600]">
+                      Your donation
+                    </p>
+                    <p
+                      class="font-poppins text-[21px] font-[700] text-[#242424]">
+                      ₦{{ amount == "" ? 0 : parseInt(amount) }}
+                    </p>
+                  </div>
+                  <div class="flex justify-between">
+                    <p class="text-[#939393] font-poppins font-[600]">
+                      Puthand Tip
+                    </p>
+                    <p
+                      class="font-poppins text-[21px] font-[700] text-[#242424]">
+                      ₦{{ tip == "" ? 0 : parseInt(tip) }}
+                    </p>
+                  </div>
+                </div>
+                <div class="flex justify-between">
+                  <p class="text-[#939393] font-poppins font-[600]">
+                    Total due
                   </p>
+                  <p class="font-poppins text-[21px] font-[700] text-[#242424]">
+                    ₦{{ parseInt(amount) + parseInt(tip) || 0 }}
+                  </p>
+                </div>
+              </div>
+              <div class="">
+                <p class="text-[#242424] text-[18px] font-[600] font-poppins">
+                  Donation Details (Optional)
+                </p>
+
+                <div class="w-full py-3">
+                  <div class="flex items-center space-x-[10px]">
+                    <div class="cursor-pointer">
+                      <div class="w-[20px] h-[20px] border rounded"></div>
+                    </div>
+                    <p class="font-poppins font-[500] text-[#999999]">
+                      Don’t display my name publicly on campaign
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
             <div class="">
               <button
+                v-if="currentPage === 3"
                 @click="payWithPaystack"
                 :class="[complete == false ? 'bg-[#939393]' : 'bg-[#295F2D]']"
                 class="w-full text-white rounded-2xl py-[12px] font-poppins font-[600]">
                 Donate Now
               </button>
+              <div class="flex justify-between">
+                <button
+                  v-if="currentPage !== 3"
+                  @click="prevSlide"
+                  class="text-[#295F2D] rounded-2xl py-[12px] font-poppins font-[600] border border-[#295F2D] px-5">
+                  previous</button
+                ><button
+                  v-if="currentPage !== 3"
+                  @click="nextSlide"
+                  class="text-white rounded-2xl py-[12px] font-poppins font-[600] bg-[#295F2D] px-5">
+                  Next
+                </button>
+              </div>
             </div>
 
             <div class="">
@@ -201,6 +226,7 @@ export default {
   components: {},
   data() {
     return {
+      currentPage: 1,
       complete: false,
       amount: 0,
       tip: 0,
@@ -214,6 +240,12 @@ export default {
   },
 
   methods: {
+    nextSlide() {
+      this.currentPage++;
+    },
+    prevSlide() {
+      this.currentPage--;
+    },
     payWithPaystack() {
       var handler = PaystackPop.setup({
         key: import.meta.env.VITE_PAYSTACK_KEY,
